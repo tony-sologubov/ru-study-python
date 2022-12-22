729e27f08a53b55fdbff6ad88e080a227de86742
Tony Sologubov <lazyswift@gmail.com>
2022-12-22 18:58:58 +0400

    lists excercise

--- lists/lists.py
+++ lists/lists.py
@@ -1,23 +1,37 @@
 class ListExercise:
     @staticmethod
     def replace(input_list: list[int]) -> list[int]:
-        """
-        Заменить все положительные элементы целочисленного списка на максимальное значение
-        элементов списка.
+        max_positive_value = 0
+        indecies_of_positive = []
+        for index, value in enumerate(input_list):
+            if value <= 0:
+                continue
+            else:
+                indecies_of_positive.append(index)
 
-        :param input_list: Исходный список
-        :return: Список с замененными элементами
-        """
-        pass
+            if value > max_positive_value:
+                max_positive_value = value
+
+        for index in indecies_of_positive:
+            input_list[index] = max_positive_value
+
+        return input_list
 
     @staticmethod
     def search(input_list: list[int], query: int) -> int:
-        """
-        Реализовать двоичный поиск
-        Функция должна возвращать индекс элемента
+        low_border = 0
+        high_border = len(input_list) - 1
 
-        :param input_list: Исходный список
-        :param query: Искомый элемент
-        :return: Номер элемента
-        """
-        pass
+        while low_border <= high_border:
+            current_index = (low_border + high_border) // 2
+            current_value = input_list[current_index]
+
+            if query == current_value:
+                return current_index
+
+            if query < current_value:
+                high_border = current_index - 1
+            else:
+                low_border = current_index + 1
+
+        return -1
